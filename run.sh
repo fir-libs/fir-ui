0b2dae81c3142bf92473e22a6b460f3ce7de4491
ibeloyar <beloyar.dev@outlook.com>
2025-02-18 17:42:39 +0300

    ci: fix extract coverage

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-COVERAGE=$(grep -oP '"lines":{"total":\d+,"covered":\d+,"skipped":0,"pct":\d+' ./coverage/coverage-summary.json)
-echo "coverage_percentage=$COVERAGE"
+COVERAGE=$(grep -oP '{"lines":{"total":\d+,"covered":\d+,"skipped":0,"pct":\d+}' ./coverage/coverage-summary.json)
+echo "coverage_percentage=$COVERAGE}"
